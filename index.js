fcf72bb4c7cca6fb3db9c1f4d7cabf18e2450194
Vishal-kumar4 <vishal8143667512@gmail.com>
2022-10-29 21:38:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+        <img src="https://www.google.com/search?q=srushti+tawde&sxsrf=ALiCzsZODw0rWuRFGjYLiX45iJUqiQabwg:1667059679750&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjGpJ-g6YX7AhVY5HMBHSaTBKMQ_AUoAXoECAEQAw#imgrc=x6SByAy3KBRjzM" alt="/" />
+    </div>
+  )
+}
