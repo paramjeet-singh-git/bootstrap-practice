dd6df3a34665ff410c17880ffbc8ba4bd949d8d8
Vishal-kumar4 <vishal8143667512@gmail.com>
2022-10-29 21:23:56 +0530

    Create chota_don.js

---
+++ chota_don.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function chota_don() {
+  return (
+    <div>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis provident vitae expedita odit deleniti nulla necessitatibus earum, voluptas possimus voluptates ut rem maxime voluptatem corrupti eos vero! Nihil aut sunt quas hic?</p>
+    </div>
+  )
+}
